577b28a934fe37d62e8504f6af0e8113162543d2
Adam Brousseau <adam.brousseau88@gmail.com>
2020-10-22 15:04:44 -0400

    Remove Hotspot from jdk15 pipeline configuration
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/jobs/configurations/jdk15u.groovy
+++ pipelines/jobs/configurations/jdk15u.groovy
@@ -1,55 +1,42 @@
 targetConfigurations = [
         "x64Mac"      : [
-                "hotspot",
                 "openj9"
         ],
         "x64MacXL"    : [
                 "openj9"
         ],
         "x64Linux"    : [
-                "hotspot",
                 "openj9"
         ],
         "x64LinuxXL"  : [
                 "openj9"
         ],
-        "x32Windows"  : [
-                "hotspot"
-        ],
         "x64Windows"  : [
-                "hotspot",
                 "openj9"
         ],
         "x64WindowsXL": [
                 "openj9"
         ],
         "ppc64Aix"    : [
-                "hotspot",
                 "openj9"
         ],
         "ppc64leLinux": [
-                "hotspot",
                 "openj9"
         ],
         "ppc64leLinuxXL": [
                 "openj9"
         ],
         "s390xLinux"  : [
-                "hotspot",
                 "openj9"
         ],
         "s390xLinuxXL": [
                 "openj9"
         ],
         "aarch64Linux": [
-                "hotspot",
                 "openj9"
         ],
         "aarch64LinuxXL": [
                 "openj9"
-        ],
-        "arm32Linux"  : [
-                "hotspot"
         ]
 ]
 

51ae518b58b57c21cd3af18e7f2a5cb499ecff58
Adam Brousseau <adam.brousseau88@gmail.com>
2020-10-22 23:27:27 -0400

    Remove hotspot from jdk16 config
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/jobs/configurations/jdk16.groovy
+++ pipelines/jobs/configurations/jdk16.groovy
@@ -1,13 +1,11 @@
 targetConfigurations = [
         "x64Mac"      : [
-                "hotspot",
                 "openj9"
         ],
         "x64MacXL"    : [
                 "openj9"
         ],
         "x64Linux"    : [
-                "hotspot",
                 "openj9"
         ],
         "x64LinuxXL"  : [
@@ -17,39 +15,25 @@ targetConfigurations = [
                 "hotspot"
         ],
         "x64Windows"  : [
-                "hotspot",
                 "openj9"
         ],
         "x64WindowsXL": [
                 "openj9"
         ],
-        "x32Windows"  : [
-                "hotspot"
-        ],
         "ppc64Aix"    : [
-                "hotspot",
                 "openj9"
         ],
         "ppc64leLinux": [
-                "hotspot",
                 "openj9"
         ],
         "ppc64leLinuxXL": [
                 "openj9"
         ],
         "s390xLinux"  : [
-                "hotspot",
                 "openj9"
         ],
         "aarch64Linux": [
-                "hotspot",
                 "openj9"
-        ],
-        "aarch64LinuxXL": [
-                "openj9"
-        ],
-        "arm32Linux"  : [
-                "hotspot"
         ]
 ]
 

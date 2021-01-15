2b717783191fa79dc0b5af844154249348ab17a6
Adam Brousseau <adam.brousseau88@gmail.com>
2021-01-15 14:46:45 -0500

    fix up 11 with node labels etc
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/jobs/configurations/jdk11u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk11u_pipeline_config.groovy
@@ -4,6 +4,7 @@ class Config11 {
                 os                  : 'mac',
                 arch                : 'x64',
                 test                : 'default',
+                additionalNodeLabels: 'ci.project.openj9 && ci.role.build && hw.arch.x86 && sw.os.osx.10_14',
                 configureArgs       : [
                         "openj9"      : '--enable-dtrace=auto --with-cmake'
                 ]
@@ -13,6 +14,7 @@ class Config11 {
                 os                   : 'mac',
                 arch                 : 'x64',
                 test                 : 'default',
+                additionalNodeLabels: 'ci.project.openj9 && ci.role.build && hw.arch.x86 && sw.os.osx.10_14',
                 additionalFileNameTag: "macosXL",
                 configureArgs        : '--with-noncompressedrefs --enable-dtrace=auto --with-cmake'
         ],
@@ -20,26 +22,44 @@ class Config11 {
         x64Linux  : [
                 os                  : 'linux',
                 arch                : 'x64',
+                additionalNodeLabels : 'ci.project.openj9 && hw.arch.x86 && sw.os.linux',
                 dockerImage         : 'adoptopenjdk/centos6_build_image',
                 dockerFile: [
                         openj9  : 'pipelines/build/dockerFiles/cuda.dockerfile'
                 ],
+                dockerNode         : 'sw.tool.docker&&sw.config.uid1000',
                 test                : 'default',
                 configureArgs       : [
                         "openj9"      : '--enable-jitserver --enable-dtrace=auto'
                 ]
         ],
 
+        x64LinuxXL    : [
+                os                   : 'linux',
+                additionalNodeLabels : 'ci.project.openj9 && hw.arch.x86 && sw.os.linux',
+                dockerImage          : 'adoptopenjdk/centos6_build_image',
+                dockerFile: [
+                        openj9  : 'pipelines/build/dockerFiles/cuda.dockerfile'
+                ],
+                dockerNode         : 'sw.tool.docker&&sw.config.uid1000',
+                arch                 : 'x64',
+                test                 : "default",
+                additionalFileNameTag: "linuxXL",
+                configureArgs        : '--with-noncompressedrefs --enable-jitserver --enable-dtrace=auto'
+        ],
+
         x64Windows: [
                 os                  : 'windows',
                 arch                : 'x64',
-                test                : 'default'
+                test                : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&sw.os.windows&&ci.role.build.release'
         ],
 
         x64WindowsXL    : [
                 os                   : 'windows',
                 arch                 : 'x64',
                 test                 : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&sw.os.windows&&ci.role.build.release',
                 additionalFileNameTag: "windowsXL",
                 configureArgs        : '--with-noncompressedrefs'
         ],
@@ -47,12 +67,14 @@ class Config11 {
         x32Windows: [
                 os                  : 'windows',
                 arch                : 'x86-32',
-                test                : 'default'
+                test                : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&sw.os.windows&&ci.role.build.release'
         ],
 
         ppc64Aix    : [
                 os                  : 'aix',
                 arch                : 'ppc64',
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.ppc64&&sw.os.aix.7_1',
                 test                : 'default'
         ],
 
@@ -60,60 +82,55 @@ class Config11 {
                 os                  : 'linux',
                 arch                : 's390x',
                 test                : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
                 configureArgs       : '--enable-dtrace=auto'
-        ],
-
-        ppc64leLinux    : [
-                os                  : 'linux',
-                arch                : 'ppc64le',
-                test                : 'default',
-                configureArgs       : [
-                        "openj9"      : '--enable-dtrace=auto --enable-jitserver'
-                ]
-
-        ],
-
-        aarch64Linux    : [
-                os                  : 'linux',
-                arch                : 'aarch64',
-                dockerImage         : 'adoptopenjdk/centos7_build_image',
-                test                : 'default',
-                configureArgs       : '--enable-dtrace=auto'
-        ],
-
-        x64LinuxXL    : [
-                os                   : 'linux',
-                dockerImage          : 'adoptopenjdk/centos6_build_image',
-                dockerFile: [
-                        openj9  : 'pipelines/build/dockerFiles/cuda.dockerfile'
-                ],
-                arch                 : 'x64',
-                test                 : "default",
-                additionalFileNameTag: "linuxXL",
-                configureArgs        : '--with-noncompressedrefs --enable-jitserver --enable-dtrace=auto'
         ],
 
         s390xLinuxXL    : [
                 os                   : 'linux',
                 arch                 : 's390x',
                 test                 : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
                 additionalFileNameTag: "linuxXL",
                 configureArgs        : '--with-noncompressedrefs --enable-dtrace=auto'
+        ],
+
+        ppc64leLinux    : [
+                os                  : 'linux',
+                arch                : 'ppc64le',
+                test                : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.ppc64le&&(sw.os.cent.7||sw.os.rhel.7)',
+                configureArgs       : [
+                        "openj9"      : '--enable-dtrace=auto --enable-jitserver'
+                ]
+
         ],
 
         ppc64leLinuxXL    : [
                 os                   : 'linux',
                 arch                 : 'ppc64le',
                 test                 : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.ppc64le&&(sw.os.cent.7||sw.os.rhel.7)',
                 additionalFileNameTag: "linuxXL",
                 configureArgs        : '--with-noncompressedrefs --enable-dtrace=auto --enable-jitserver'
+        ],
+
+        aarch64Linux    : [
+                os                  : 'linux',
+                arch                : 'aarch64',
+                dockerImage         : 'adoptopenjdk/centos7_build_image',
+                test                : false,
+                additionalNodeLabels : 'ci.project.openj9 && hw.arch.aarch64 && sw.os.linux',
+                configureArgs       : '--enable-dtrace=auto'
         ],
 
         aarch64LinuxXL    : [
                 os                   : 'linux',
                 dockerImage          : 'adoptopenjdk/centos7_build_image',
+                dockerNode         : 'sw.tool.docker',
                 arch                 : 'aarch64',
-                test                 : 'default',
+                test                 : false,
+                additionalNodeLabels : 'ci.project.openj9 && hw.arch.aarch64 && sw.os.linux',
                 additionalFileNameTag: "linuxXL",
                 configureArgs        : '--with-noncompressedrefs --enable-dtrace=auto'
         ],
@@ -121,11 +138,12 @@ class Config11 {
         riscv64Linux      :  [
                 os                   : 'linux',
                 dockerImage          : 'adoptopenjdk/centos6_build_image',
+                dockerNode         : 'sw.tool.docker&&sw.config.uid1000',
                 arch                 : 'riscv64',
                 crossCompile         : 'x64',
                 buildArgs            : '--cross-compile',
                 configureArgs        : '--disable-ddr --openjdk-target=riscv64-unknown-linux-gnu --with-sysroot=/opt/fedora28_riscv_root'
-        ],
+        ]
   ]
 
 }

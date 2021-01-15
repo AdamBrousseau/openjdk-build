2f26dbfae6514493b9c9f5cfcaecb7b8b636a058
Adam Brousseau <adam.brousseau88@gmail.com>
2021-01-15 14:46:45 -0500

    fix up 11 with node labels etc
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/jobs/configurations/jdk11u_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk11u_pipeline_config.groovy
@@ -7,6 +7,7 @@ class Config11 {
                 additionalFileNameTag: [
                         "openj9"    : "mixedrefs"
                 ],
+                additionalNodeLabels: 'ci.project.openj9 && ci.role.build && hw.arch.x86 && sw.os.osx.10_14',
                 configureArgs       : [
                         "openj9"      : '--enable-dtrace=auto --with-cmake --with-mixedrefs'
                 ]
@@ -15,10 +16,12 @@ class Config11 {
         x64Linux  : [
                 os                  : 'linux',
                 arch                : 'x64',
+                additionalNodeLabels : 'ci.project.openj9 && hw.arch.x86 && sw.os.linux',
                 dockerImage         : 'adoptopenjdk/centos6_build_image',
                 dockerFile: [
                         openj9  : 'pipelines/build/dockerFiles/cuda.dockerfile'
                 ],
+                dockerNode         : 'sw.tool.docker&&sw.config.uid1000',
                 test                : 'default',
                 additionalFileNameTag: [
                         "openj9"    : "mixedrefs"
@@ -37,7 +40,8 @@ class Config11 {
                 additionalFileNameTag: [
                         "openj9"    : "mixedrefs"
                 ],
-                test                : 'default'
+                test                : 'default',
+                additionalNodeLabels : 'ci.project.openj9&&sw.os.windows&&ci.role.build.release'
         ],
 
         ppc64Aix    : [
@@ -50,6 +54,7 @@ class Config11 {
                         "openj9"    : "mixedrefs"
                 ],
                 cleanWorkspaceAfterBuild: true
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.ppc64&&sw.os.aix.7_1',
                 test                : 'default'
         ],
 
@@ -62,7 +67,9 @@ class Config11 {
                 ],
                 configureArgs       : [
                         "openj9"      : '--enable-dtrace=auto --with-mixedrefs'
-                ]
+                ],
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
+
         ],
 
         ppc64leLinux    : [
@@ -72,6 +79,7 @@ class Config11 {
                 additionalFileNameTag: [
                         "openj9"    : "mixedrefs"
                 ],
+                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.ppc64le&&(sw.os.cent.7||sw.os.rhel.7)',
                 configureArgs       : [
                         "openj9"      : '--enable-dtrace=auto --enable-jitserver --with-mixedrefs'
                 ]
@@ -91,18 +99,18 @@ class Config11 {
                 ]
                 arch                 : 'x64',
                 test                 : "default",
-                additionalFileNameTag: "linuxXL",
-                configureArgs        : '--with-noncompressedrefs --enable-jitserver --enable-dtrace=auto'
+                additionalNodeLabels : 'ci.project.openj9 && hw.arch.aarch64 && sw.os.linux'
         ],
 
         riscv64Linux      :  [
                 os                   : 'linux',
                 dockerImage          : 'adoptopenjdk/centos6_build_image',
+                dockerNode         : 'sw.tool.docker&&sw.config.uid1000',
                 arch                 : 'riscv64',
                 crossCompile         : 'x64',
                 buildArgs            : '--cross-compile',
                 configureArgs        : '--disable-ddr --openjdk-target=riscv64-unknown-linux-gnu --with-sysroot=/opt/fedora28_riscv_root'
-        ],
+        ]
   ]
 
 }

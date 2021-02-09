e9f4ca7b4e64bce1c60ef19d1085538f97285976
Adam Brousseau <adam.brousseau88@gmail.com>
2021-02-08 22:41:10 -0500

    Convert jdk16 to mixed refs except win/aix
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/jobs/configurations/jdk16_pipeline_config.groovy
+++ pipelines/jobs/configurations/jdk16_pipeline_config.groovy
@@ -5,16 +5,8 @@ class Config16 {
                 arch                : 'x64',
                 additionalNodeLabels: 'ci.project.openj9 && ci.role.build && hw.arch.x86 && sw.os.osx.10_14',
                 test                : 'default',
-                configureArgs       : '--enable-dtrace'
-        ],
-
-        x64MacXL    : [
-                os                   : 'mac',
-                arch                 : 'x64',
-                additionalNodeLabels: 'ci.project.openj9 && ci.role.build && hw.arch.x86 && sw.os.osx.10_14',
-                test                 : 'default',
-                additionalFileNameTag: "macosXL",
-                configureArgs        : '--with-noncompressedrefs --enable-dtrace'
+                additionalFileNameTag: "mixedrefs",
+                configureArgs       : '--enable-dtrace --with-mixedrefs'
         ],
 
         x64Linux  : [
@@ -32,26 +24,10 @@ class Config16 {
                 additionalTestLabels: [
                         openj9      : '!(sw.os.cent.6||sw.os.rhel.6)'
                 ],
+                additionalFileNameTag: "mixedrefs",
                 configureArgs       : [
-                        "openj9"      : '--enable-dtrace --enable-jitserver'
+                        "openj9"      : '--enable-dtrace --enable-jitserver --with-mixedrefs'
                 ]
-        ],
-
-        x64LinuxXL  : [
-                os                   : 'linux',
-                arch                 : 'x64',
-                additionalNodeLabels : 'ci.project.openj9 && hw.arch.x86 && sw.os.linux',
-                dockerImage          : 'adoptopenjdk/centos7_build_image',
-                dockerFile: [
-                        openj9  : 'pipelines/build/dockerFiles/cuda.dockerfile'
-                ],
-                dockerNode         : 'sw.tool.docker&&sw.config.uid1000',
-                test                 : 'default',
-                additionalTestLabels: [
-                        openj9      : '!(sw.os.cent.6||sw.os.rhel.6)'
-                ],
-                additionalFileNameTag: "linuxXL",
-                configureArgs        : '--with-noncompressedrefs --enable-dtrace --enable-jitserver'
         ],
 
         x64Windows: [
@@ -84,16 +60,8 @@ class Config16 {
                 arch                : 's390x',
                 test                : 'default',
                 additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
-                configureArgs       : '--enable-dtrace'
-        ],
-
-        s390xLinuxXL  : [
-                os                   : 'linux',
-                arch                 : 's390x',
-                test                 : 'default',
-                additionalFileNameTag: "linuxXL",
-                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
-                configureArgs        : '--with-noncompressedrefs --enable-dtrace'
+                additionalFileNameTag: "mixedrefs",
+                configureArgs       : '--enable-dtrace --with-mixedrefs'
         ],
 
         ppc64leLinux    : [
@@ -101,19 +69,11 @@ class Config16 {
                 arch                : 'ppc64le',
                 additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
                 test                : 'default',
+                additionalFileNameTag: "mixedrefs",
                 configureArgs       : [
-                        "openj9"      : '--enable-dtrace --enable-jitserver'
+                        "openj9"      : '--enable-dtrace --enable-jitserver --with-mixedrefs'
                 ]
 
-        ],
-
-        ppc64leLinuxXL    : [
-                os                   : 'linux',
-                arch                 : 'ppc64le',
-                additionalNodeLabels : 'ci.project.openj9&&ci.role.build.release&&hw.arch.s390x&&(sw.os.cent.7||sw.os.rhel.7)',
-                test                 : 'default',
-                additionalFileNameTag: "linuxXL",
-                configureArgs        : '--with-noncompressedrefs --disable-ccache --enable-dtrace'
         ],
 
         aarch64Linux    : [
@@ -123,18 +83,8 @@ class Config16 {
                 dockerNode         : 'sw.tool.docker',
                 additionalNodeLabels : 'ci.project.openj9 && hw.arch.aarch64 && sw.os.linux',
                 test                : false,
-                configureArgs       : '--enable-dtrace'
-        ],
-
-        aarch64LinuxXL    : [
-                os                   : 'linux',
-                dockerImage          : 'adoptopenjdk/centos7_build_image',
-                arch                 : 'aarch64',
-                dockerNode         : 'sw.tool.docker',
-                additionalNodeLabels : 'ci.project.openj9 && hw.arch.aarch64 && sw.os.linux',
-                test                 : false,
-                additionalFileNameTag: "linuxXL",
-                configureArgs        : '--with-noncompressedrefs --enable-dtrace'
+                additionalFileNameTag: "mixedrefs",
+                configureArgs       : '--enable-dtrace --with-mixedrefs'
         ]
   ]
 
